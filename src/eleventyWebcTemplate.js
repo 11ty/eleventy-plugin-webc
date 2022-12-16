884bb87a88a2440d51dac5c08dbcc1db13f9b132
Zach Leatherman <zachleatherman@gmail.com>
2022-12-16 11:15:42 -0600

    This plugin has been significantly simplified with new features in 2.0.0-canary.19

--- src/eleventyWebcTemplate.js
+++ src/eleventyWebcTemplate.js
@@ -4,7 +4,6 @@ const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const CompileString = EleventyRenderPlugin.String;
 
 const CodeManager = require("./codeManager.js");
-const WebCIncremental = require("./incremental.js");
 const BundleAssetsToContent = require("./bundleAssets.js");
 
 function relativePath(inputPath, newGlob) {
@@ -25,9 +24,9 @@ module.exports = function(eleventyConfig, options = {}) {
 
 	let cssManager = new CodeManager();
 	let jsManager = new CodeManager();
-	let incremental = new WebCIncremental();
+
+	let _WebC;
 	let componentsMap = false; // cache the glob search
-	let componentsMapKey;
 	let moduleScript;
 
 	eleventyConfig.on("eleventy.before", async () => {
@@ -37,11 +36,10 @@ module.exports = function(eleventyConfig, options = {}) {
 		// For ESM in CJS
 		let { WebC, ModuleScript } = await import("@11ty/webc");
 		moduleScript = ModuleScript;
-		incremental.setWebC(WebC);
+		_WebC = WebC;
 
 		if(options.components) {
 			componentsMap = WebC.getComponentsMap(options.components);
-			componentsMapKey = JSON.stringify(componentsMap);
 		}
 	});
 
@@ -63,37 +61,10 @@ module.exports = function(eleventyConfig, options = {}) {
 		eleventyConfig.addFilter(options.filters.js, (url, bucket) => getJs(url, bucket));
 	}
 
-	eleventyConfig.on("eleventy.layouts", layouts => {
-		incremental.setLayouts(layouts);
-	});
-
 	eleventyConfig.addExtension("webc", {
 		outputFileExtension: "html",
 
-		isIncrementalMatch: function (incrementalFilePath) {
-			// Eleventy layouts don’t appear directly in the WebC component graph, so we use the `eleventy.layouts` map here
-			if(incremental.isFileUsingLayout(this.inputPath, incrementalFilePath)) {
-				return true;
-			}
-
-			let {page, setup} = incremental.get(this.inputPath);
-
-			if(page && setup) {
-				let components = page.getComponents(setup);
-				if(components.includes(incrementalFilePath)) {
-					return true;
-				}
-			}
-
-			return false;
-		},
-
 		compileOptions: {
-			cache: true,
-			getCacheKey: function(contents, inputPath) {
-				// if global components change, recompile!
-				return contents + inputPath + componentsMapKey;
-			},
 			permalink: function(contents, inputPath) {
 				return (data) => {
 					return moduleScript.evaluateScript("permalink", contents, {
@@ -105,7 +76,10 @@ module.exports = function(eleventyConfig, options = {}) {
 		},
 
 		compile: async function(inputContent, inputPath) {
-			let page = incremental.add(inputContent, inputPath);
+			let page = new _WebC();
+
+			page.setBundlerMode(true);
+			page.setContent(inputContent, inputPath);
 
 			if(componentsMap) {
 				page.defineComponents(componentsMap);
@@ -138,20 +112,19 @@ module.exports = function(eleventyConfig, options = {}) {
 				let setupObject = {
 					data,
 				};
+
 				if(data.webc?.components) {
-					let WebC = incremental.webc;
-					setupObject.components = WebC.getComponentsMap(relativePath(data.page.inputPath, data.webc.components));
+					setupObject.components = _WebC.getComponentsMap(relativePath(data.page.inputPath, data.webc.components));
 				}
 
 				if(options.before && typeof options.before === "function") {
 					await options.before(page);
 				}
 
-				let setup = await page.setup(setupObject);
-				incremental.addSetup(inputPath, setup);
+				let { html, css, js, buckets, components } = await page.compile(setupObject);
 
-				let { ast, serializer } = setup;
-				let { html, css, js, buckets } = await serializer.compile(ast);
+				// 2.0.0-canary.19+
+				this.addDependencies(inputPath, components);
 
 				cssManager.addToPage(data.page.url, css, "default");
 
